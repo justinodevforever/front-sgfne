af0ab9cde557e939dd3640e69866a0c7460f83ad
justinodevforever <jchitombi7@gmail.com>
2024-04-23 21:24:27 +0100

    al3

--- src/components/routers/messagens/Chat.jsx
+++ src/components/routers/messagens/Chat.jsx
@@ -171,9 +171,9 @@ function Chat() {
             </div>
           </div>
         )}
-        {/* {users?.map((u) => (
+        {users?.map((u) => (
           <ContactChat contact={u} key={u?.id} />
-        ))} */}
+        ))}
       </div>
     </>
   );
